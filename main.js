a02ae9a02657ab42a5dd927bbd1c1eca8fef2a4d
Manuel Valbuena <valbuenamoralesmanuelalejandro@gmail.com>
2023-10-09 11:42:30 -0500

    Cambio de logo. Se añaden nuevas cards. Se cambia la información de detalle de producto y su imagen

--- main.js
+++ main.js
@@ -68,18 +68,78 @@ const productList = []
 
 productList.push({
     name:'Bike',
+    price: 125,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+})
+productList.push({
+    name:'Bike',
     price: 120,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    image: 'https://images.pexels.com/photos/9651659/pexels-photo-9651659.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
 })
 productList.push({
-    name:'Pantalla',
-    price: 220,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    name:'Bike',
+    price: 140,
+    image: 'https://images.pexels.com/photos/545004/pexels-photo-545004.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
 })
 productList.push({
-    name:'Computador',
-    price: 620,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    name:'Bike',
+    price: 130,
+    image: 'https://images.pexels.com/photos/15009946/pexels-photo-15009946/free-photo-of-an-electric-bicycle-in-a-park-lane.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+})
+productList.push({
+    name:'Bike',
+    price: 150,
+    image: 'https://images.pexels.com/photos/10873458/pexels-photo-10873458.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+})
+productList.push({
+    name:'Bike',
+    price: 135,
+    image: 'https://images.pexels.com/photos/12880825/pexels-photo-12880825.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+})
+productList.push({
+    name:'Bike',
+    price: 145,
+    image: 'https://images.pexels.com/photos/255934/pexels-photo-255934.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+})
+productList.push({
+    name:'Bike',
+    price: 160,
+    image: 'https://images.pexels.com/photos/101980/pexels-photo-101980.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+})
+productList.push({
+    name:'Bike',
+    price: 170,
+    image: 'https://images.pexels.com/photos/101666/pexels-photo-101666.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+})
+productList.push({
+    name:'Bike',
+    price: 155,
+    image: 'https://images.pexels.com/photos/1414466/pexels-photo-1414466.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+})
+productList.push({
+    name:'Bike',
+    price: 165,
+    image: 'https://images.pexels.com/photos/756079/pexels-photo-756079.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+})
+productList.push({
+    name:'Bike',
+    price: 140,
+    image: 'https://images.pexels.com/photos/12308116/pexels-photo-12308116.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+})
+productList.push({
+    name:'Bike',
+    price: 130,
+    image: 'https://images.pexels.com/photos/1209480/pexels-photo-1209480.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+})
+productList.push({
+    name:'Bike',
+    price: 150,
+    image: 'https://images.pexels.com/photos/4485054/pexels-photo-4485054.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+})
+productList.push({
+    name:'Bike',
+    price: 135,
+    image: 'https://images.pexels.com/photos/7706555/pexels-photo-7706555.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
 })
 
 function renderProducts(arr) {
